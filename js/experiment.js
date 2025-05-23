63d216b713fdba2ef26c08e16bad538adf7d06d5
Amanda Popadich <popadich@uw.edu>
2025-05-23 11:50:11 -0700

    u

--- js/experiment.js
+++ js/experiment.js
@@ -1,18 +1,36 @@
 var stimuli = [];
 
+// Function for shuffling order of the data//
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+}
+
+//Function for sampling from random order of data//
+function random_sample(arr, n) {
+    shuffled = shuffleArray(arr)
+    return shuffled = arr.slice(0, n):
+}
+
+//Loads the csv data//
 Papa.parse('data_csv/believe_wiki.csv', {
 download: true,
 header: true,
 complete: function(results) {
-    stimuli = results.data.map(row => ({
+    const allStimuli = results.data.map(row => ({
     target: `<p>${row.target}</p>`,
     context: `<p>${row.context}</p>`
     }));
+    stimuli = random_sample(allStimuli, 60)
     console.log('Stimuli loaded:', stimuli);
     startExperiment(); 
 }
 });
 
+//Run experiment procedure
 function startExperiment() {
   const jsPsych = initJsPsych({ display_element: 'jspsych-target' }); 
 
