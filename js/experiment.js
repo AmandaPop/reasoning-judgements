8a1a1a6eb2c9fb30a03fa871a51a3ace128c90b8
Amanda Popadich <popadich@uw.edu>
2025-05-23 11:18:05 -0700

    creating experiment site

--- js/experiment.js
+++ js/experiment.js
@@ -1,8 +1,40 @@
-const jsPsych = initJsPsych();
+var stimuli = [];
 
-const hello_trial = {
-    type: jsPsychHtmlKeyboardResponse,
-    stimulus: 'Hello world!'
+Papa.parse('data_csv/think_wiki.csv', {
+download: true,
+header: true,
+complete: function(results) {
+    stimuli = results.data.map(row => ({
+    target: `<p>${row.target}</p>`,
+    context: `<p>${row.context}</p>`
+    }));
+
+    startExperiment(); 
 }
+});
 
-jsPsych.run([hello_trial]);
+function startExperiment() {
+var trial = {
+    type: jsPsychHtmlSliderResponse,
+    stimulus: function() {
+    return jsPsych.timelineVariable('target');
+    },
+    prompt: function() {
+    return jsPsych.timelineVariable('context');
+    },
+    labels: ['0', '100'],
+    require_movement: true,
+    button_label: 'Continue'
+};
+
+var trial_procedure = {
+    timeline: [trial],
+    timeline_variables: stimuli,
+    randomize_order: true
+};
+
+var timeline = [];
+timeline.push(trial_procedure);
+
+jsPsych.run(timeline);
+}
