5736fec6fda5b9bfacda8baa142a5f8f660d0481
Amanda Popadich <popadich@uw.edu>
2025-06-30 10:34:15 -0700

    u

--- js/experiment.js
+++ js/experiment.js
@@ -40,7 +40,7 @@ Papa.parse('data_csv/believe.csv', {
       }
     });
 
-    console.log('Stimuli loaded:', stimuli);
+    //console.log('Stimuli loaded:', stimuli);
     stimuliLoaded = true;
     maybeStartExperiment(); 
   }
@@ -69,7 +69,7 @@ Papa.parse('data_csv/fillers_believe.csv', {
       }
     });
 
-    console.log('Fillers loaded:', fillers);
+    //console.log('Fillers loaded:', fillers);
     fillersLoaded = true;
     maybeStartExperiment(); 
   }
