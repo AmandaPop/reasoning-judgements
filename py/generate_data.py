1434e888a183a4f2df775ecb1a713f1788384824
Amanda Popadich <popadich@uw.edu>
2025-07-09 10:55:11 -0700

    u

--- py/generate_data.py
+++ py/generate_data.py
@@ -16,13 +16,13 @@ def write_csv(filename, sents):
         writer.writeheader()
         writer.writerows(sents)
 
-nps = [('I', 1), ('You', 2), ('Sally', 3)] 
+nps = ['I', 'You', 'Sally'] 
 verbs = ['believe', 'think']
 modals = ['can', 'could', 'might', 'will', 'would', 'should', 'shall', 'ought to', '']
 P = [('the sky is red', 0), ('the sky is blue', 1)]
 q = 'went for a run'
 conditional = [True, False]
-contexts = ['QUD_weak', 'QUD_strong' 'None']
+contexts = ['QUD_weak', 'QUD_strong']
 
 
 conditions = [i for i in itertools.product(nps, verbs, modals, P, conditional, contexts)]
@@ -31,18 +31,18 @@ data = []
 for condition in conditions:
     #print(condition)
     np, verb, modal, p, conditional, context = condition
-    if np[0] == 'Sally':
+    if np == 'Sally':
         person = 3
-    elif np[0] == 'You':
+    elif np == 'You':
         person = 2
     else:
         person = 1
     if person == 3 and modal == '':
-        sentence = f'{np[0]} {modal}{verb}s {p[0]}.'
+        sentence = f'{np} {modal}{verb}s {p[0]}.'
     else:
-        sentence = f'{np[0]} {modal} {verb} {p[0]}.'
+        sentence = f'{np} {modal} {verb} {p[0]}.'
     if conditional:
-        sentence = f"If {np[0]} {verb} {p[0]}, then {np[0]} {q}."
+        sentence = f"If {np} {verb} {p[0]}, then {np[0]} {q}."
     if context == 'QUD_weak':
         if person == 1:
             c = f'Do you {verb} {p[0]}?'
@@ -57,8 +57,6 @@ for condition in conditions:
             c = f'Does she know {p[0]}?'
         else:
             c = f'Do I know {p[0]}?'
-    if context == 'None':
-        c = ''
     factP = p[1]
 
     stimulus = {
