977154be803b25ba76df33d85d5124f1740dce62
Amanda Popadich <popadich@uw.edu>
2025-07-10 13:25:28 -0700

    update sentences

--- py/generate_data.py
+++ py/generate_data.py
@@ -1,6 +1,7 @@
 import itertools
 import sys
 import csv
+import random
 
 file = sys.argv[1]
 
@@ -18,63 +19,130 @@ def write_csv(filename, sents):
         writer.writeheader()
         writer.writerows(sents)
 
-nps = ['I', 'Sally'] 
+person = ['1', '3'] 
+nouns3 = [
+    "Alice", "Benjamin", "Charlotte", "Daniel", "Emily",
+    "Frank", "Grace", "Henry", "Isabella", "Jack",
+    "Katherine", "Liam", "Mia", "Noah", "Olivia",
+    "Peter", "Quinn", "Rachel", "Samuel", "Tara",
+    "Victoria", "William", "Xander", 'Sally',
+    "Zachary", "Andrew", "Bella", "Caleb", "Daisy",
+    "Ethan", "Fiona", "George", "Hannah", "Ian",
+    "Julia", "Kyle", "Laura", "Matthew", "Natalie",
+    "Oscar", "Paige", "Ryan", "Sophie", "Thomas",
+    "Violet", "Wesley"
+    ]
 verbs = ['believe', 'think']
 modals = ['can', 'must', '']
-P = [('the sky is red', 0), ('the sky is blue', 1)]
+
+P = [0, 1]
+#considering to add: ('that about sums everything up', 2),
+trueP = [('the sky is blue', 1),
+      ("people need air to breathe", 1),
+      ('the moon is in the sky', 1),
+      ('a triangle has 3 sides', 1),
+      ('grass is green', 1),
+      ('people need to eat to survive', 1),
+      ('people drink water when they are thirsty', 1),
+      ('people write with pencils', 1),
+      ('hats are worn on your head', 1),
+      ('most airplanes fly faster than cars drive', 1),
+      ('rain falls from the clouds in the sky', 1),
+      ('the internet allows people to communicate across the world', 1),
+      ('refrigerators are a good way to keep food cold', 1),
+      ('electricity is what powers a lightbulb', 1),
+      ('earth takes one year to orbit the sun', 1),
+      ('gravity pulls objects towards the earth', 1),
+      ('language is used to help people communicate', 1),
+      ('water freezes at 0 degrees celcius', 1),
+      ('birds migrate to warmer places during winter', 1),
+      ('children learn to walk before they learn to run and jump', 1),
+      ('the human heart is meant for pumping blood through the body', 1),
+      ("oceans cover more than 70 percent of the earth's surface", 1),
+      ('either it will rain tomorrow or it will not rain tomorrow', 1),
+      ('a circle is round', 1)]
+falseP = [
+      ("the earth is flat", 0),
+      ("the sun rises in the west", 0),
+      ("water is dry", 0),
+      ("birds have fur", 0),
+      ("fish can live on land without water", 0),
+      ("snow is hot", 0),
+      ("fire is cold", 0),
+      ("the moon is made of cheese", 0),
+      ("trees can walk", 0),
+      ("people can see through walls", 0),
+      ("rain falls upward", 0),
+      ("cats can speak English", 0),
+      ("a triangle has four sides", 0),
+      ("elephants are the size of ants", 0),
+      ("books grow on trees", 0),
+      ("the ocean is made of lemonade", 0),
+      ("time moves backward at night", 0),
+      ("spiders have six legs", 0),
+      ("humans have three eyes", 0),
+      ("clouds are made of cotton", 0),
+      ("humans can photosynthesize like plants", 0),
+      ("the brain is located in the stomach", 0),
+      ('The capital of France is Madrid.', 0),
+      ('Mount Everest is located in the United States.', 0)
+    ]
 q = 'went for a run'
 #conditional = [True, False]
 contexts = ['QUD_weak', 'QUD_strong']
 
 
-conditions = [i for i in itertools.product(nps, verbs, modals, P, contexts)]
+conditions = [i for i in itertools.product(person, verbs, modals, P, contexts)]
 
 data = []
 for condition in conditions:
-    np, verb, modal, p, context = condition
-    if np == 'Sally':
-        person = 3
-    elif np == 'You':
-        person = 2
+    person, verb, modal, p, context = condition
+    if p == 0:
+        choice = random.choice(falseP)
+        falseP.remove(choice)
+        clause = choice[0]
+    elif p == 1:
+        choice = random.choice(trueP)
+        trueP.remove(choice)
+        clause = choice[0]
+    if person == '3':
+        #pick a random noun to use, then remove it so we don't use it again
+        np = random.choice(nouns3)
+        nouns3.remove(np)
     else:
-        person = 1
+        np = 'I'
         #sally thinks/beleives P
-    if person == 3 and modal == '':
-        sentence = f'{np} {verb}s {p[0]}.'
-        si = f"{np} {verb}s but doesn't know {p[0]}."
-        no_si = f"{np} {verb}s and possibly knows {p[0]}."
-    #I/You think/believe P
-    elif person != 3 and modal == '':
-        sentence = f'{np} {verb} {p[0]}.'
-        si = f"{np} {verb} but don't know {p[0]}."
-        no_si = f"{np} {verb} and possibly know {p[0]}."
+    if person == '3' and modal == '':
+        sentence = f'{np} {verb}s {clause}.'
+        si = f"{np} {verb}s but doesn't know {clause}."
+        no_si = f"{np} {verb}s and possibly knows {clause}."
+    #I think/believe P
+    elif person != '3' and modal == '':
+        sentence = f'{np} {verb} {clause}.'
+        si = f"{np} {verb} but don't know {clause}."
+        no_si = f"{np} {verb} and possibly know {clause}."
     elif person == 3:
     #Sally can think/believe P
-        sentence = f'{np} {modal} {verb} {p[0]}.'
-        si = f"{np} {modal} {verb} but doesn't know {p[0]}."
-        no_si = f"{np} {modal} {verb} and possibly know {p[0]}."
+        sentence = f'{np} {modal} {verb} {clause}.'
+        si = f"{np} {modal} {verb} but doesn't know {clause}."
+        no_si = f"{np} {modal} {verb} and possibly know {clause}."
     #I/You can think/believe P
     else:
-        sentence = f'{np} {modal} {verb} {p[0]}.'
-        si = f"{np} {modal} {verb} but don't know {p[0]}."
-        no_si = f"{np} {modal} {verb} and possibly know {p[0]}."
+        sentence = f'{np} {modal} {verb} {clause}.'
+        si = f"{np} {modal} {verb} but don't know {clause}."
+        no_si = f"{np} {modal} {verb} and possibly know {clause}."
     # if conditional:
     #     sentence = f"If {np} {verb} {p[0]}, then {np[0]} {q}."
     if context == 'QUD_weak':
-        if person == 1:
-            c = f'Do you {verb} {p[0]}?'
-        elif person == 3:
-            c = f'Does Sally {verb} {p[0]}?'
-        else:
-            c = f'Do I {verb} {p[0]}?'
+        if person == '1':
+            c = f'Do you {verb} {clause}?'
+        elif person == '3':
+            c = f'Does {np} {verb} {clause}?'
     if context == 'QUD_strong':
-        if person == 1:
-            c = f'Do you know {p[0]}?'
-        elif person == 3:
-            c = f'Does she know {p[0]}?'
-        else:
-            c = f'Do I know {p[0]}?'
-    factP = p[1]
+        if person == '1':
+            c = f'Do you know {clause}?'
+        elif person == '3':
+            c = f'Does she know {clause}?'
 
 
     stimulus = {
@@ -83,7 +151,7 @@ for condition in conditions:
         'No_SI' : no_si,
         'verb' : verb,
         'context': c,
-        'factP' : factP,
+        'factP' : p,
         'modal' : modal,
         'person' : person,
         #'conditional' : conditional
